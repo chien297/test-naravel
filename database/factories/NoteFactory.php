715af4dca2c23fed9843467d57866032331add19
chienlv <chienlv@smarosc.com>
2022-05-06 11:41:55 +0700

    day la test

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Note;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+     protected $model = Note::class;
+    public function definition()
+    {
+        return [
+            //
+            'name' => $this->faker->name,
+            'text' => $this->faker->text
+
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Note;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Note::factory()
+        ->times(100)
+        ->create();
     }
 }
